cc4d6c0f7a8ef0987edf4719093081ec912b0080
John Lim <JohnLim@g.harvard.edu>
2014-09-13 23:26:38 -0400

    Updated readme file, added a index with car 13SEP2014 JLim

---
+++ README.md
@@ -0,0 +1,4 @@
+#Hello-World
+Created by: John Lim
+
+Example of version control workflow
---
+++ index.php
@@ -0,0 +1,3 @@
+<h1>Hello World!</h1>
+
+<img src='images/ferrari-ego-concept.jpg' id="car" height="50%" width="50%">
